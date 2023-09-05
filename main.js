6d9b8419ac115a285be23ccff536b94d6c7315ff
Grnetsky <1101767893@qq.com>
2023-09-05 21:07:11 +0800

    feat: 基于map的多窗口管理

--- main.js
+++ main.js
@@ -7,6 +7,7 @@ const Store = require('electron-store')
 const store = new Store()
 let windows = []
 
+const winsMap = new Map()
 const winTheLock = app.requestSingleInstanceLock();  //给应用加抢占琐  即使用单例模式保证同一个app只能打开一个
 
 if(winTheLock){
@@ -20,7 +21,7 @@ if(winTheLock){
         }
     })
 
-    function createNewWindow(url,parent = null) {
+    function createNewWindow(windowName,url) {
         // 主窗口 相关设置参见 https://www.electronjs.org/zh/docs/latest/api/browser-window#new-browserwindowoptions
         let window = new BrowserWindow({
             title:'当html文件没有title标签时则显示此title', //优先级： HTML title>BrowserWindow>package.json name属性 > Electron 默认   另外还提供了setTitle方法 来动态改变窗口标题
@@ -31,13 +32,13 @@ if(winTheLock){
             minWidth: 300, //窗口的最小宽度
             minHeight: 200, // 窗口的最小高度
             resizable: true, // 是否可以缩放窗口
-            frame:false, // 隐藏窗口标题栏，菜单栏以及边框
-            parent:parent?parent:null, // 设置父窗口引用
+            frame:false, // false隐藏窗口标题栏，菜单栏以及边框
+            parent:null, // 设置父窗口引用
             x: 400,
             y: 600, // 在屏幕中的位置 默认为正中间
             icon: path.join(__dirname,'logo.jpg'), // 指定标题栏图标 默认使用应用可执行文件的图片作为图标
             transparent: false, // 设置窗口透明
-            alwaysOnTop: true, // 是否常置于顶层
+            alwaysOnTop: false, // 是否常置于顶层
             webPreferences:{
                 nodeIntegration:true, //允许渲染进程调用nodejs模块
                 contextIsolation: false,
@@ -51,7 +52,22 @@ if(winTheLock){
         window.on("close", function () {
             window = null
         })
-        return window
+        winsMap.set(windowName,window)
+    }
+
+    function closeWindowByName(windowName) {
+        const window = winsMap.get(windowName)
+        if(window){
+            window.close()
+        }else{
+            console.log(`${windowName} not existed`)
+        }
+    }
+
+    function closeAllWindows() {
+        winsMap.forEach((win)=>{
+            win.close()
+        })
     }
     // 监听所有窗口关闭事件
     app.on('window-all-closed',()=>{
@@ -64,94 +80,17 @@ if(winTheLock){
             protocol: 'file',
             pathname: path.join(__dirname,'window1/index.html')
         })
-        let parentWindow = createNewWindow(url1)
-        windows.push(parentWindow)
-
+        createNewWindow('window1',url1)
         // 第二个渲染进程的窗口文件路径
         const url2 = url.format({
             protocol: 'file',
             pathname: path.join(__dirname,'window2/index.html')
         })
         // 创建第二个窗口
-        setTimeout(
-            ()=>{
-                windows.push(createNewWindow(url2,parentWindow))  // 第二个参数传入父窗口的引用 子窗口随父窗口一起移动 关闭等等
-            },2000)
+        createNewWindow('window2',url2)
+        setTimeout(closeAllWindows,2000,)
+        // setTimeout(closeWindowByName,2000,'window2')
     })
 }else {
     app.quit() // 退出app
-}
-
-
-ipcMain.on('system-message',(event, args)=>{
-    console.log('i am from Renderer')
-})
-ipcMain.on('devTool',(event, args)=>{
-        if(args){
-            window.webContents.openDevTools()
-        }else {
-            window.webContents.closeDevTools()
-        }
-    }
-)
-
-// 消息对应的窗口引用
-const messageChannelMap = {}
-
-
-/*
-* @description 注册消息窗口 ： 消息名：[窗口列表]
-* @param channel 消息名
-* @param webContentId 窗口id
-*
-* */
-function registMessageChannel(channel,webContentId) {
-    if(messageChannelMap[channel] !== undefined){  // 存在该消息列表
-        let alreadyHas = false
-        for(let i =0;i<messageChannelMap[channel][i].length;i++){
-            if(messageChannelMap[channel][i] === webContentId){  // 判断该列表是否有该窗口id
-                alreadyHas = true
-            }
-            if(!alreadyHas){
-                messageChannelMap[channel].push(webContentId)
-            }
-        }
-    }else {
-        // 若不存在该消息 则创建该消息列表
-        messageChannelMap[channel] = [webContentId]  // TODO 这里似乎只能注册一个？
-    }
-}
-
-
-// 监听注册消息事件
-ipcMain.on('transMessage',(event, channel,data)=>{
-    try{
-        // 发送数据
-    transMessage(getMessageChannel(channel),channel,data)
-    }catch (e) {
-        console.log(e)
-    }
-})
-
-ipcMain.on('registMessageChannel',(event, data)=>{
-    try{
-        // 注册
-        registMessageChannel(data,event.sender.id)
-    }catch (e) {
-        console.log(e)
-    }
-})
-
-function transMessage(webContents,channel,data) {
-    for(let i = 0;i<webContents.length;i++){
-        for(let j = 0 ;j<windows.length;j++){
-            if(webContents[i] === windows[i].webContents.id){
-                windows[j].webContents.send(channel,data)
-            }
-        }
-    }
-}
-
-function getMessageChannel(channel) {
-    return messageChannelMap[channel] || []
 }
